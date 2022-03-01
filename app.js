361cc26fc6931c578f7c344c7bc7b4d7d32af46b
varunsahi62 <varunsahi62@gmail.com>
2022-03-01 16:16:48 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,96 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "It's simply writing down your thoughts and feelings to understand them more clearly. And if you struggle with stress, depression, or anxiety, keeping a journal can be a great idea. It can help you gain control of your emotions and improve your mental health.";
+
+const aboutContent = "Hi, My name is Varun Sahi & I'm a full stack web developer. I was never really interested in anything, didn't have any ambition or a hobby until got into Web Development and got instantly into it. Creating a Web Site or a Web Applicaion gives me so much satisfaction and the feeling of being good at something. Although I'm just a beginner but I'm ready to learn & grow.";
+
+const contactContent = "You can contact me on the following platforms: ";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin-varunsahi:12345@cluster0.u4vnu.mongodb.net/blogDB");
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.post("/delete", function(req, res){
+
+  const postID = req.body.postID;
+  
+  Post.findByIdAndRemove(postID, function(err){
+    if(!err){
+      console.log("item deleted");
+    }
+  });
+
+  res.redirect("/");
+
+});
+
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.listen(3000, function() {
+  console.log("Server has started successfully!");
+});
